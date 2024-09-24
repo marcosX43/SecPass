bcf9a2efcd9c73a2b5fd6b35dbd7007f3c908cbb
Anurag <33193568+marcosX43@users.noreply.github.com>
2024-09-24 21:37:15 +0530

    README.md Added

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup,find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='SecurePassX',
@@ -13,5 +16,8 @@ setup(
         'console_scripts': [
             'SecurePassX=SecurePassX.__main__:main',  # Command to execute your main() function
         ],
-    }
+    },
+    description="A secure password management solution",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
 )
